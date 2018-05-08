453373562814908500ce2dcc28f4a28b5c2e6c87
Uri <oriol.castroarnau@gmail.com>
2018-05-08 19:00:36 +0200

    Added OpenGraph meta tags and made the footer sticky

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import { withPrefix } from 'gatsby-link'
 
 import './all.sass'
 
@@ -10,14 +11,30 @@ import AppFooter from '../components/Footer'
 import Config from '../../meta/config'
 
 const TemplateWrapper = ({ children }) => (
-  <div>
+  <div className="my-site">
     <Helmet>
       <title>{Config.siteTitle}</title>
       <meta name="description" content={Config.siteDescription} />
+      <meta name="keywords" content="Esplai, Esplac, Lleure" />
+      <meta name="msapplication-TileColor" content="#00A2A7" />
+      <meta name="theme-color" content="#00A2A7" />
+      <meta property="og:locale" content="ca_ES" />
+      <meta property="og:image" content="/img/og-image.jpg" />
+      <meta property="og:image:height" content="301" />
+      <meta property="og:image:width" content="576" />
+      <meta property="og:type" content="website" />
+      <meta property="og:description" content={Config.siteDescription} />
+      <meta property="og:title" content={Config.siteTitle} />
+      <meta property="og:url" content={Config.siteUrl} />
+      <meta name="twitter:card" content="img/og-image.jpg" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href={withPrefix('/icons/apple-touch-icon.png')}
+      />
     </Helmet>
     <Navigation />
-
-    {children()}
+    <div className="my-site-content">{children()}</div>
 
     <AppFooter />
   </div>
